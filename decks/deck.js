97f3c67209f5c0ce88cc90ea39ea947c6e174551
Christian Mejia <chrismejia10@gmail.com>
2020-07-22 00:30:47 -0400

    todo: code-surfer things added, but broken

--- decks/deck.js
+++ decks/deck.js
@@ -1,5 +1,13 @@
 import { slides as firstDeck } from './first-deck.mdx'
 import { slides as themes } from './themes.mdx'
 import { slides as errors } from './errors.mdx'
+import { slides as steps } from './steps.mdx'
+import { slides as compilation } from './code-surfer-compilation.mdx'
 
-export const slides = [...firstDeck, ...themes, ...errors]
+export const slides = [
+  ...firstDeck,
+  ...themes,
+  ...errors,
+  ...compilation,
+  ...steps,
+]
